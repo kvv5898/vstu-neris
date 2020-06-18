dd62ca52bed7617f495c918d65232ed618f5f45f
KVV <KVV@DESKTOP-U80DEHH>
2020-06-18 16:04:44 +0300

    init(FilterConfig filterConfig)

--- src/main/java/src/filter/ServletFilter.java
+++ src/main/java/src/filter/ServletFilter.java
@@ -3,6 +3,7 @@ package src.filter;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletException;
@@ -48,6 +49,12 @@ public class ServletFilter implements Filter {
 				chain.doFilter(request, response);
 			}
 		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
